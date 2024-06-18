bfded2a6d672030c60ed97993dfa652bb9193f91
shumax <maxcastro.mv@gmail.com>
2024-06-18 17:33:31 -0300

    feat: cors policy

--- server/src/utils/response.ts
+++ server/src/utils/response.ts
@@ -1,6 +1,11 @@
 import { ServerResponse } from 'node:http';
 
 export default function sendResponse(res: ServerResponse, statusCode: number, data: any) {
-  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.writeHead(statusCode, { 
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, OPTIONS',
+    'access-control-max-age': '2592000',
+    'Content-Type': 'application/json'
+  });
   res.end(JSON.stringify(data));
 }
